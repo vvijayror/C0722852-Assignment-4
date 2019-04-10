0aa8355b70c09d6cdd7b7aafa65e4a8f5cca9ee1
visualstudio <visualstudio@122-PC23>
2019-04-10 11:27:44 -0400

    commit

--- C0722852-Assignment 4/Program.cs
+++ C0722852-Assignment 4/Program.cs
@@ -10,6 +10,34 @@ namespace C0722852_Assignment_4
     {
         static void Main(string[] args)
         {
+            ArrayList Beowulf;
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                p.Beowulf = new ArrayList();
+
+            }
+            public void Run() { this.ReadTextFiles(); }
+
+            public void ReadTextFiles()
+            {
+                using (StreamReader file = new StreamReader("U:/Users/722852/beowulf.txt"))
+                {
+                    int counter = 0;
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(ln);
+                        Beowulf.Add(ln);
+                    }
+                    file.Close();
+                    Console.WriteLine($"File has {counter} lines.");
+                }
+            }
+
         }
+    }
+}
     }
 }
