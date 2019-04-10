4b05eb22691a9ca01715af7298cbc9830996f7bd
visualstudio <visualstudio@122-PC23>
2019-04-10 11:56:50 -0400

    Section b

--- C0722852-Assignment 4/Program.cs
+++ C0722852-Assignment 4/Program.cs
@@ -26,17 +26,24 @@ namespace C0722852_Assignment_4
             {
                 using (StreamReader file = new StreamReader("U:/Users/722852/beowulf.txt"))
                 {
-                    int counter = 0;
-                    string ln;
 
-                    while ((ln = file.ReadLine()) != null)
+                int counter = 0;
+                int number = 0;
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains("Fare") == true && line.Contains("War") == false || line.Contains("Fare") == true && line.Contains("War") == false)
                     {
-                        Console.WriteLine(ln);
-                
-                        Beowulf.Add(ln);
+                        int x = counter - 1;
+                        number++;
                     }
-                    file.Close();
-                    Console.WriteLine($"File has {counter} lines.");
+                    counter++;
+                }
+                Console.WriteLine($"The number of lines that contains *Fare* without the word *War* are {number}");
+                file.Close();
+
+
+                Console.WriteLine($"File has {counter} lines.");
                 }
             }
 
